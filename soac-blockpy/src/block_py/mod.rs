3b14ef0e40060e798765c58ef81bc9cb165eadc8
Adam Hupp <adam@hupp.org>
2026-04-02 18:53:02 -0700

    Remove redundant CoreCallLikeExpr marker

--- soac-blockpy/src/block_py/mod.rs
+++ soac-blockpy/src/block_py/mod.rs
@@ -668,10 +668,6 @@ pub enum CoreNumberLiteralValue {
     Float(f64),
 }
 
-pub(crate) trait CoreCallLikeExpr: Sized + Instr {}
-
-impl CoreCallLikeExpr for CoreBlockPyExprWithAwaitAndYield {}
-
 impl Instr for CoreBlockPyExprWithAwaitAndYield {
     type Name = UnresolvedName;
 }
@@ -819,8 +815,6 @@ impl TryMapExpr<CoreBlockPyExprWithYield, CoreBlockPyExprWithAwaitAndYield>
     }
 }
 
-impl CoreCallLikeExpr for CoreBlockPyExprWithYield {}
-
 impl Instr for CoreBlockPyExprWithYield {
     type Name = UnresolvedName;
 }
@@ -914,8 +908,6 @@ impl TryMapExpr<CoreBlockPyExpr, CoreBlockPyExprWithYield> for CoreBlockPyExprWi
         }
     }
 }
-
-impl<N: BlockPyNameLike> CoreCallLikeExpr for CoreBlockPyExpr<N> {}
 
 impl<N: BlockPyNameLike> Instr for CoreBlockPyExpr<N> {
     type Name = N;
@@ -1050,8 +1042,6 @@ where
     }
 }
 
-impl CoreCallLikeExpr for CodegenBlockPyExpr {}
-
 impl Instr for CodegenBlockPyExpr {
     type Name = LocatedName;
 }
@@ -1155,7 +1145,7 @@ pub(crate) fn core_call_expr_with_meta<E>(
     keywords: Vec<CoreBlockPyKeywordArg<E>>,
 ) -> E
 where
-    E: CoreCallLikeExpr + From<Call<E>>,
+    E: Instr + From<Call<E>>,
 {
     Call::new(func, args, keywords)
         .with_meta(Meta::new(node_index, range))
@@ -1170,7 +1160,7 @@ pub(crate) fn core_named_call_expr_with_meta<E>(
     keywords: Vec<CoreBlockPyKeywordArg<E>>,
 ) -> E
 where
-    E: CoreCallLikeExpr + From<Call<E>> + From<Load<E>>,
+    E: Instr + From<Call<E>> + From<Load<E>>,
     InstrName<E>: From<ast::ExprName>,
 {
     let func = load_name_expr(ExprName {
@@ -1207,7 +1197,7 @@ pub(crate) fn core_positional_call_expr_with_meta<E>(
     args: Vec<E>,
 ) -> E
 where
-    E: CoreCallLikeExpr + From<Call<E>> + From<Load<E>>,
+    E: Instr + From<Call<E>> + From<Load<E>>,
 {
     core_named_call_expr_with_meta(
         func_name,
@@ -1228,13 +1218,13 @@ pub(crate) fn core_runtime_named_call_expr_with_meta<E>(
     keywords: Vec<CoreBlockPyKeywordArg<E>>,
 ) -> E
 where
-    E: CoreCallLikeExpr + Instr<Name = UnresolvedName> + From<Call<E>> + From<Load<E>>,
+    E: Instr<Name = UnresolvedName> + From<Call<E>> + From<Load<E>>,
 {
     let func = core_runtime_name_expr_with_meta(func_name, node_index.clone(), range);
     core_call_expr_with_meta(func, node_index, range, args, keywords)
 }
 
-pub(crate) fn core_runtime_positional_call_expr_with_meta<E: CoreCallLikeExpr>(
+pub(crate) fn core_runtime_positional_call_expr_with_meta<E>(
     func_name: &str,
     node_index: ast::AtomicNodeIndex,
     range: ruff_text_size::TextRange,
